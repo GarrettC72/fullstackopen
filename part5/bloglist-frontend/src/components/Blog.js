d4de4a82dd810bae0ea7bcbc02d3d0f337190385
Garrett Chen <garrettchen2@gmail.com>
2023-02-28 22:02:41 -0500

    Completed 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false)
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const extraBlogDetails = () => {
+    return (
+      <div>
+        <a href={blog.url}>{blog.url}</a>
+        <div>likes {blog.likes}<button>like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setFullView(!fullView)}>
+          {fullView ? 'hide' : 'view'}
+        </button>
+      </div>
+      {fullView && extraBlogDetails()}
+    </div> 
+  )
+}
 
 export default Blog
