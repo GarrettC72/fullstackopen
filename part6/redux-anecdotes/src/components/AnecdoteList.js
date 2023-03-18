57671b078b1b64f5ca17a34a1db65eb3ad8e38f9
Garrett Chen <garrettchen2@gmail.com>
2023-03-18 04:06:48 -0400

    Completed 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -17,7 +17,11 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    return anecdotes.filter(
+      anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  })
 
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
 
